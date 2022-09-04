d355097715159cb6a3b4c2fc0afa2cd08c6ad984
Your Name <you@example.com>
2022-09-03 22:51:51 -0300

    terminado

--- src/client/components/tasks/TaskItem.jsx
+++ src/client/components/tasks/TaskItem.jsx
@@ -25,10 +25,8 @@ export const TaskItem = ({data, openModalEditTodo}) => {
             <h3 className="card-title">{data.title}</h3>
             <h5>{data.description}</h5>
             <div className="btn-group">
-                <button className="btn btn-outline btn-error" onClick={() => getTaskByIdAndDelete(data._id)}>Detele</button>
+                <button className="btn btn-outline btn-error" onClick={() => getTaskByIdAndDelete(data._id)}>Delete</button>
                 <button className="btn btn-outline btn-warning" onClick={ openModalEditTodo } onClickCapture={() => getTask(data._id)}>Edit</button>
-                <button className="btn btn-outline btn-success">Complete</button>
-                <button className="btn btn-outline btn-info">Incomplete</button>
             </div>
         </div>
     </div>
